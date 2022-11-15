16c6af4e86e67a3e9d8d58f9df1f5eaedd85c9cc
Morgan Taylor <marymorg@broadinstitute.org>
2022-11-15 10:27:29 -0500

    import the proper generated class

--- service/src/main/java/bio/terra/pipelines/iam/SamService.java
+++ service/src/main/java/bio/terra/pipelines/iam/SamService.java
@@ -3,8 +3,10 @@ package bio.terra.pipelines.iam;
 import bio.terra.common.iam.BearerToken;
 import bio.terra.common.sam.SamRetry;
 import bio.terra.common.sam.exception.SamExceptionFactory;
-// import bio.terra.pipelines.model.SystemStatusSystems;
+import bio.terra.pipelines.generated.model.ApiSystemStatusSystems;
+import java.util.List;
 import org.broadinstitute.dsde.workbench.client.sam.ApiException;
+import org.broadinstitute.dsde.workbench.client.sam.model.SystemStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,24 +38,24 @@ public class SamService {
     }
   }
 
-  //    public SystemStatusSystems status() {
-  //        // No access token needed since this is an unauthenticated API.
-  //        try {
-  //            // Don't retry status check
-  //            SystemStatus samStatus = samClient.statusApi().getSystemStatus();
-  //            var result = new SystemStatusSystems().ok(samStatus.getOk());
-  //            var samSystems = samStatus.getSystems();
-  //            // Populate error message if Sam status is non-ok
-  //            if (result.isOk() == null || !result.isOk()) {
-  //                String errorMsg = "Sam status check failed. Messages = " + samSystems;
-  //                logger.error(errorMsg);
-  //                result.addMessagesItem(errorMsg);
-  //            }
-  //            return result;
-  //        } catch (Exception e) {
-  //            String errorMsg = "Sam status check failed";
-  //            logger.error(errorMsg, e);
-  //            return new SystemStatusSystems().ok(false).messages(List.of(errorMsg));
-  //        }
-  //    }
+  public ApiSystemStatusSystems status() {
+    // No access token needed since this is an unauthenticated API.
+    try {
+      // Don't retry status check
+      SystemStatus samStatus = samClient.statusApi().getSystemStatus();
+      var result = new ApiSystemStatusSystems().ok(samStatus.getOk());
+      var samSystems = samStatus.getSystems();
+      // Populate error message if Sam status is non-ok
+      if (result.isOk() == null || !result.isOk()) {
+        String errorMsg = "Sam status check failed. Messages = " + samSystems;
+        logger.error(errorMsg);
+        result.addMessagesItem(errorMsg);
+      }
+      return result;
+    } catch (Exception e) {
+      String errorMsg = "Sam status check failed";
+      logger.error(errorMsg, e);
+      return new ApiSystemStatusSystems().ok(false).messages(List.of(errorMsg));
+    }
+  }
 }
