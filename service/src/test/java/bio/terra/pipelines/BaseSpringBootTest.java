c621af84846bcc79813108c662823fbeedc53f91
Morgan Taylor <marymorg@broadinstitute.org>
2022-11-22 10:53:17 -0500

    remove contextconfiguration, fixes to application-test.yml

--- service/src/test/java/bio/terra/pipelines/BaseSpringBootTest.java
+++ service/src/test/java/bio/terra/pipelines/BaseSpringBootTest.java
@@ -2,9 +2,8 @@ package bio.terra.pipelines;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ActiveProfiles({"test", "human-readable-logging"})
-@ContextConfiguration(classes = App.class)
+// @ContextConfiguration(classes = App.class)
 public abstract class BaseSpringBootTest {}
