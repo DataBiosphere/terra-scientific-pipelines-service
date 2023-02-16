354b46d4ba0c4bb9f2dbfb93758fd3a468f3ee1d
Morgan Taylor <marymorg@broadinstitute.org>
2023-02-16 13:46:06 -0500

    store message to satisfy SonarCloud check

--- service/src/main/java/bio/terra/pipelines/db/JobsDao.java
+++ service/src/main/java/bio/terra/pipelines/db/JobsDao.java
@@ -70,20 +70,17 @@ public class JobsDao {
       jdbcTemplate.update(sql, params);
       logger.info("Inserted record for job {}", jobUuid);
     } catch (DuplicateKeyException e) {
-      // Note we call valueOf() to handle the possible NPE if getMessage() returns null,
-      // since a null check was not sufficient to satisfy SonarCloud's static analysis.
-      if (e.getMessage() != null) {
-        if (e.getMessage()
-            .contains("duplicate key value violates unique constraint \"jobs_pkey\"")) {
-          // Job with job_id already exists.
-          throw new DuplicateObjectException(
-              String.format(
-                  "Job with id %s already exists - pipelineId %s submitted on %s",
-                  jobUuid, job.getPipelineId(), job.getTimeSubmitted()),
-              e);
-        } else {
-          throw e;
-        }
+      String message = e.getMessage();
+      if (message != null
+          && message.contains("duplicate key value violates unique constraint \"jobs_pkey\"")) {
+        // Job with job_id already exists.
+        throw new DuplicateObjectException(
+            String.format(
+                "Job with id %s already exists - pipelineId %s submitted on %s",
+                jobUuid, job.getPipelineId(), job.getTimeSubmitted()),
+            e);
+      } else {
+        throw e;
       }
     }
     return jobUuid;
