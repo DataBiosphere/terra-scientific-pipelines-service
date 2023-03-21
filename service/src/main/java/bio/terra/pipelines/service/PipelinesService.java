9853ec6b7182457d91379d90acc2988e232ab0b6
M. Morgan Aster <marymorg@broadinstitute.org>
2023-03-21 15:41:01 -0400

    [TSPS-31] refactor pipelineId check to be called from controller layer (#20)
    * refactor pipelineId check to be called from controller layer and not invoke an exception
    
    * remove PipelinesService from JobsService, update tests for pipelineExists method
    
    * fix inputs to ResponseEntity for 404
    
    * add PipelinesService mock to JobsControllerApi
    
    * return 404 by throwing PipelineNotFoundException in the controller layer

--- service/src/main/java/bio/terra/pipelines/service/PipelinesService.java
+++ service/src/main/java/bio/terra/pipelines/service/PipelinesService.java
@@ -1,7 +1,6 @@
 package bio.terra.pipelines.service;
 
 import bio.terra.pipelines.db.PipelinesDao;
-import bio.terra.pipelines.db.exception.PipelineNotFoundException;
 import bio.terra.pipelines.service.model.Pipeline;
 import java.util.List;
 import org.slf4j.Logger;
@@ -26,12 +25,7 @@ public class PipelinesService {
     return pipelinesDao.getPipelines();
   }
 
-  public void validatePipeline(String pipelineId) {
-    logger.info("Validate pipeline");
-
-    boolean pipelineExists = pipelinesDao.checkPipelineExists(pipelineId);
-    if (!pipelineExists) {
-      throw new PipelineNotFoundException(String.format("Pipeline %s not found.", pipelineId));
-    }
+  public boolean pipelineExists(String pipelineId) {
+    return pipelinesDao.checkPipelineExists(pipelineId);
   }
 }
