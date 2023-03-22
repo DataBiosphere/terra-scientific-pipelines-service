914a5cb89d3102e58083a99b85926c654181aa94
Aaron Hatcher <hatcher@broadinstitute.org>
2023-03-22 17:04:40 -0400

    updating tests to work with new JobsService changes

--- service/src/test/java/bio/terra/pipelines/service/JobsServiceTest.java
+++ service/src/test/java/bio/terra/pipelines/service/JobsServiceTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.*;
 
 import bio.terra.pipelines.db.JobsDao;
 import bio.terra.pipelines.db.exception.DuplicateObjectException;
-import bio.terra.pipelines.db.exception.PipelineNotFoundException;
 import bio.terra.pipelines.service.model.Job;
 import bio.terra.pipelines.testutils.BaseUnitTest;
 import java.util.UUID;
@@ -18,13 +17,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 class JobsServiceTest extends BaseUnitTest {
 
   @Autowired private JobsService jobsService;
-  @MockBean private PipelinesService pipelinesService;
   @MockBean private JobsDao jobsDao;
 
   // parameters used repeatedly by various tests, and things we'll want to mocks to respond to
   // universally
   private final String testUserId = "testUser";
-  private final String testBadPipelineId = "testBadPipeline";
   private final String testGoodPipelineId = "testGoodPipeline";
   private final String testPipelineVersion = "testPipelineVersion";
 
@@ -34,12 +31,6 @@ class JobsServiceTest extends BaseUnitTest {
 
   @BeforeEach
   void initMocks() {
-    // pipeline service validation throws on the bad id and does nothing on the good id
-    doThrow(PipelineNotFoundException.class)
-        .when(pipelinesService)
-        .validatePipeline(testBadPipelineId);
-    doNothing().when(pipelinesService).validatePipeline(testGoodPipelineId);
-
     // dao throws on job containing bad id and returns good uuid on job containing good uuid
     when(jobsDao.createJob(argThat((Job j) -> j.getJobId() == testBadUUID)))
         .thenThrow(DuplicateObjectException.class);
@@ -53,10 +44,9 @@ class JobsServiceTest extends BaseUnitTest {
 
   // JobsService.createJob has 3 pieces of business logic to check.
   // Case 1: It creates a job object.  Can test this by ensuring that it calls the dao with a valid
-  // Job object and
-  // doesn't fail if the dao doesn't.  Beyond not throwing, verify that it returns the same UUID
-  // Case 2: It validates that a pipeline exists, so we want to verify it fails when one doesn't
-  // Case 3: If an error occurs while writing the job object, it should pass through the error
+  // Job object and  doesn't fail if the dao doesn't.  Beyond not throwing, verify that it returns
+  // the same UUID
+  // Case 2: If an error occurs while writing the job object, it should pass through the error
   @Test
   void testCreateJob_successfulWriteUUIDsMatch() {
     // override a bit of our bean with a spy here, which leaves the rest untouched
@@ -65,13 +55,6 @@ class JobsServiceTest extends BaseUnitTest {
 
     UUID writtenUUID = jobServiceSpy.createJob(testUserId, testGoodPipelineId, testPipelineVersion);
     assertEquals(writtenUUID, testGoodUUID);
-  }
-
-  @Test
-  void testCreateJob_pipelineDoesNotExist() {
-    assertThrows(
-        PipelineNotFoundException.class,
-        () -> jobsService.createJob(testUserId, testBadPipelineId, testPipelineVersion));
   }
 
   @Test
