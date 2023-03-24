894c70acb2d913e90be78906a81b53f5d5eedc0b
Aaron Hatcher <hatcher@broadinstitute.org>
2023-03-24 17:46:53 -0400

    test data correct loaded for duplicate test

--- service/src/test/java/bio/terra/pipelines/db/JobsDaoTest.java
+++ service/src/test/java/bio/terra/pipelines/db/JobsDaoTest.java
@@ -2,6 +2,7 @@ package bio.terra.pipelines.db;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import bio.terra.pipelines.db.exception.DuplicateObjectException;
 import bio.terra.pipelines.service.model.Job;
 import java.time.Instant;
 import java.util.UUID;
@@ -12,11 +13,12 @@ class JobsDaoTest extends BaseDaoTest {
 
   @Autowired JobsDao jobsDao;
 
+  private final String testPresentJobId = "deadbeef-dead-beef-deaf-beefdeadbeef";
   private final String testUserId = "testUser";
   private final String testPipelineId = "testPipeline";
 
   @Test
-  void testWriteJob() {
+  void testWriteValidJob() {
     UUID jobId = UUID.randomUUID();
     Instant timeSubmitted = Instant.now();
 
@@ -27,4 +29,19 @@ class JobsDaoTest extends BaseDaoTest {
 
     assertEquals(jobId, uuidResult);
   }
+
+  @Test
+  void testWriteDuplicateJob() {
+    // repeating the exact same logic as above, except using a job id that we know is preloaded in the test data
+    UUID jobId = UUID.fromString(testPresentJobId);
+    Instant timeSubmitted = Instant.now();
+
+    String status = "SUBMITTED";
+    Job jobFull =
+        new Job(jobId, testUserId, testPipelineId, "testVersion", timeSubmitted, null, status);
+
+    //    List<Job> jobs = jobsDao.getJobs(testUserId, testPipelineId);
+    assertThrows(DuplicateObjectException.class, () -> jobsDao.createJob(jobFull));
+  }
+
 }
