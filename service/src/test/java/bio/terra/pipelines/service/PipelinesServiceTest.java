6d0cea787675b82f374e00e6dd959476e4e581de
Morgan Taylor <marymorg@broadinstitute.org>
2023-03-27 15:26:58 -0400

    add test for getPipelines() in PipelinesService

--- service/src/test/java/bio/terra/pipelines/service/PipelinesServiceTest.java
+++ service/src/test/java/bio/terra/pipelines/service/PipelinesServiceTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import bio.terra.pipelines.db.PipelinesDao;
+import bio.terra.pipelines.service.model.Pipeline;
 import bio.terra.pipelines.testutils.BaseUnitTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +14,21 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 class PipelinesServiceTest extends BaseUnitTest {
   @Autowired private PipelinesService pipelinesService;
   @MockBean private PipelinesDao mockPipelinesDao;
+
+  @Test
+  void testGetPipelines() {
+    // getPipelines should return a list of Pipelines
+    Pipeline testPipeline1 =
+        new Pipeline("testPipeline1", "Test Pipeline One", "Test Pipeline One Description");
+    Pipeline testPipeline2 =
+        new Pipeline("testPipeline2", "Test Pipeline Two", "Test Pipeline Two Description");
+    List<Pipeline> pipelinesList = List.of(testPipeline1, testPipeline2);
+    when(mockPipelinesDao.getPipelines()).thenReturn(pipelinesList);
+
+    List<Pipeline> returnedPipelines = pipelinesService.getPipelines();
+    assertEquals(2, returnedPipelines.size());
+    assertTrue(returnedPipelines.containsAll(pipelinesList));
+  }
 
   @Test
   void testPipelineExists_true() {
