4e30b99b71ed7beda0a1956598137282c32f4ae6
Aaron Hatcher <hatcher@broadinstitute.org>
2023-04-05 14:34:30 -0400

    Happy path validated.  Error paths not working, as additional config is required to add in (I believe) ControllerAdvice classes that handle exceptions

--- service/src/test/java/bio/terra/pipelines/controller/JobsControllerTest.java
+++ service/src/test/java/bio/terra/pipelines/controller/JobsControllerTest.java
@@ -1,8 +1,10 @@
 package bio.terra.pipelines.controller;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,11 +14,18 @@ import bio.terra.common.iam.SamUser;
 import bio.terra.common.iam.SamUserFactory;
 import bio.terra.pipelines.app.controller.JobsApiController;
 import bio.terra.pipelines.config.SamConfiguration;
+import bio.terra.pipelines.db.exception.PipelineNotFoundException;
+import bio.terra.pipelines.generated.model.ApiPostJobRequestBody;
 import bio.terra.pipelines.iam.SamService;
 import bio.terra.pipelines.service.JobsService;
 import bio.terra.pipelines.service.PipelinesService;
 import bio.terra.pipelines.service.model.Job;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
@@ -30,10 +39,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ContextConfiguration(classes = JobsApiController.class)
-@WebMvcTest
+@WebMvcTest()
 class JobsControllerTest {
-  @MockBean JobsService serviceMock;
-  @MockBean PipelinesService pipelinesService;
+  @MockBean JobsService jobsServiceMock;
+  @MockBean PipelinesService pipelinesServiceMock;
   @MockBean SamUserFactory samUserFactoryMock;
   @MockBean BearerTokenFactory bearerTokenFactory;
   @MockBean SamConfiguration samConfiguration;
@@ -79,7 +88,7 @@ class JobsControllerTest {
   @Test
   void testGetMessageOk() throws Exception {
 
-    when(serviceMock.getJob(testUser.getSubjectId(), pipelineId, jobIdOkDone.toString()))
+    when(jobsServiceMock.getJob(testUser.getSubjectId(), pipelineId, jobIdOkDone.toString()))
         .thenReturn(jobOkDone);
 
     mockMvc
@@ -87,6 +96,59 @@ class JobsControllerTest {
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     //        .andExpect(content().json(jobOkDone.toString())); // this not working
+  }
+
+  @Test
+  void testCreateJobGoodPipeline() throws Exception {
+    // This makes the body of the post... which is a lot for very little
+    ApiPostJobRequestBody postBody = new ApiPostJobRequestBody().pipelineVersion("TestVersion");
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+    ObjectWriter ow = mapper.writer();
+    String postBodyAsJson = ow.writeValueAsString(postBody);
+
+    UUID fakeJobId = UUID.randomUUID();
+
+    // the mocks
+    // createJob(String userId, String pipelineId, String pipelineVersion)
+    when(jobsServiceMock.createJob(testUser.getSubjectId(), pipelineId, "TestVersion"))
+        .thenReturn(fakeJobId);
+    when(pipelinesServiceMock.pipelineExists(pipelineId)).thenReturn(true);
+
+    // the crafting the expected response json
+    Map<String, UUID> expectedRespponseMap = new HashMap<>();
+    expectedRespponseMap.put("jobId", fakeJobId);
+    String expectedResponseJson = ow.writeValueAsString(expectedRespponseMap);
+    mockMvc
+        .perform(
+            post(String.format("/api/jobs/v1alpha1/%s", pipelineId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(postBodyAsJson))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(content().string(expectedResponseJson));
+  }
+
+  @Test
+  void testCreateJobBadPipeline() throws Exception {
+    // This makes the body of the post... which is a lot for very little
+    ApiPostJobRequestBody postBody = new ApiPostJobRequestBody().pipelineVersion("TestVersion");
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+    ObjectWriter ow = mapper.writer();
+    String postBodyAsJson = ow.writeValueAsString(postBody);
+    // the mocks
+    when(pipelinesServiceMock.pipelineExists(pipelineId)).thenReturn(false);
+
+    // currently failing
+    mockMvc
+        .perform(
+            post(String.format("/api/jobs/v1alpha1/%s", pipelineId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(postBodyAsJson))
+        .andExpect(
+            result ->
+                assertTrue(result.getResolvedException() instanceof PipelineNotFoundException));
   }
 
   // this test not working - to be fixed later (TSPS-9)
