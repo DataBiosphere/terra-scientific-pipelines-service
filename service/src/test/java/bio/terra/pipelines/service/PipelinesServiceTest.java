68d555a5006ab0929d4a060ef6bed39a436d5ff1
Aaron Hatcher <hatcher@broadinstitute.org>
2023-04-10 14:52:58 -0400

    merging

--- service/src/test/java/bio/terra/pipelines/service/PipelinesServiceTest.java
+++ service/src/test/java/bio/terra/pipelines/service/PipelinesServiceTest.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import bio.terra.pipelines.db.PipelinesDao;
+import bio.terra.pipelines.service.model.Pipeline;
 import bio.terra.pipelines.testutils.BaseUnitTest;
+import bio.terra.pipelines.testutils.MockMvcUtils;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,9 +17,21 @@ class PipelinesServiceTest extends BaseUnitTest {
   @MockBean private PipelinesDao mockPipelinesDao;
 
   @Test
+  void testGetPipelines() {
+    // getPipelines should return a list of Pipelines
+    List<Pipeline> pipelinesList =
+        List.of(MockMvcUtils.TEST_PIPELINE_1, MockMvcUtils.TEST_PIPELINE_2);
+    when(mockPipelinesDao.getPipelines()).thenReturn(pipelinesList);
+
+    List<Pipeline> returnedPipelines = pipelinesService.getPipelines();
+    assertEquals(2, returnedPipelines.size());
+    assertTrue(returnedPipelines.containsAll(pipelinesList));
+  }
+
+  @Test
   void testPipelineExists_true() {
     // when validating an existing pipeline, should return true
-    String existingPipelineId = "existingPipeline";
+    String existingPipelineId = MockMvcUtils.TEST_PIPELINE_ID_1;
     when(mockPipelinesDao.checkPipelineExists(existingPipelineId)).thenReturn(true);
 
     assertTrue(pipelinesService.pipelineExists(existingPipelineId));
