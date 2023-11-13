7e48d7b710816b81a8ff477fe597df304ba014db
Jose Soto <jsoto@broadinstitute.org>
2023-11-13 10:08:14 -0500

    remove debug logging message that was testing leonardo service

--- service/src/main/java/bio/terra/pipelines/app/controller/PipelinesApiController.java
+++ service/src/main/java/bio/terra/pipelines/app/controller/PipelinesApiController.java
@@ -1,15 +1,12 @@
 package bio.terra.pipelines.app.controller;
 
 import bio.terra.pipelines.db.entities.Pipeline;
-import bio.terra.pipelines.dependencies.leonardo.LeonardoService;
 import bio.terra.pipelines.generated.api.PipelinesApi;
 import bio.terra.pipelines.generated.model.ApiPipeline;
 import bio.terra.pipelines.generated.model.ApiPipelinesGetResult;
 import bio.terra.pipelines.service.PipelinesService;
 import io.swagger.annotations.Api;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,16 +17,11 @@ import org.springframework.stereotype.Controller;
 @Api(tags = {"pipelines"})
 public class PipelinesApiController implements PipelinesApi {
   private final PipelinesService pipelinesService;
-  private final LeonardoService leonardoService;
 
   @Autowired
-  public PipelinesApiController(
-      PipelinesService pipelinesService, LeonardoService leonardoService) {
+  public PipelinesApiController(PipelinesService pipelinesService) {
     this.pipelinesService = pipelinesService;
-    this.leonardoService = leonardoService;
   }
-
-  private static final Logger logger = LoggerFactory.getLogger(PipelinesApiController.class);
 
   // -- Pipelines --
 
@@ -37,8 +29,6 @@ public class PipelinesApiController implements PipelinesApi {
   public ResponseEntity<ApiPipelinesGetResult> getPipelines() {
     List<Pipeline> pipelineList = pipelinesService.getPipelines();
     ApiPipelinesGetResult result = pipelinesToApi(pipelineList);
-    // remove this before merge, was used to test we could actually get a result
-    logger.info(leonardoService.getApps(false).toString());
 
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
