f009ec41b9fcb1af51954ba2175df2e366ce8b8e
Jose Soto <jsoto@broadinstitute.org>
2023-11-14 14:14:32 -0500

    add a few more tests for samservice

--- service/src/test/java/bio/terra/pipelines/dependencies/sam/SamServiceTest.java
+++ service/src/test/java/bio/terra/pipelines/dependencies/sam/SamServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import bio.terra.pipelines.dependencies.common.HealthCheck;
+import bio.terra.pipelines.generated.model.ApiSystemStatusSystems;
 import org.broadinstitute.dsde.workbench.client.sam.ApiException;
 import org.broadinstitute.dsde.workbench.client.sam.api.StatusApi;
 import org.broadinstitute.dsde.workbench.client.sam.model.SystemStatus;
@@ -19,18 +20,26 @@ class SamServiceTest {
     SamClient samClient = mock(SamClient.class);
     StatusApi statusApi = mock(StatusApi.class);
 
-    SystemStatus systemStatus = new SystemStatus();
-    systemStatus.setOk(true);
+    SystemStatus expectedSystemStatus = new SystemStatus();
+    expectedSystemStatus.setOk(true);
 
     when(samClient.statusApi()).thenReturn(statusApi);
-    when(statusApi.getSystemStatus()).thenReturn(systemStatus);
+    when(statusApi.getSystemStatus()).thenReturn(expectedSystemStatus);
 
     SamService samService = spy(new SamService(samClient));
 
     HealthCheck.Result actualResult = samService.checkHealth();
 
     assertEquals(
-        new HealthCheck.Result(systemStatus.getOk(), systemStatus.toString()), actualResult);
+        new HealthCheck.Result(expectedSystemStatus.getOk(), expectedSystemStatus.toString()),
+        actualResult);
+
+    ApiSystemStatusSystems apiSystemStatusSystems = samService.checkHealthApiSystemStatus();
+    assertEquals(
+        new ApiSystemStatusSystems()
+            .ok(expectedSystemStatus.getOk())
+            .addMessagesItem(expectedSystemStatus.toString()),
+        apiSystemStatusSystems);
   }
 
   @Test
@@ -53,5 +62,12 @@ class SamServiceTest {
     HealthCheck.Result actualResult = samService.checkHealth();
 
     assertEquals(expectedResultOnFail, actualResult);
+
+    ApiSystemStatusSystems apiSystemStatusSystems = samService.checkHealthApiSystemStatus();
+    assertEquals(
+        new ApiSystemStatusSystems()
+            .ok(expectedResultOnFail.isOk())
+            .addMessagesItem(expectedResultOnFail.message()),
+        apiSystemStatusSystems);
   }
 }
