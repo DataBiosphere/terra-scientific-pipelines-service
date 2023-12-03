0e50f9b299cee887ed9e6d0f333f66fb0da8dc74
Jose Soto <jsoto@broadinstitute.org>
2023-12-03 13:27:00 -0500

    code smell

--- service/src/main/java/bio/terra/pipelines/app/common/MetricsUtils.java
+++ service/src/main/java/bio/terra/pipelines/app/common/MetricsUtils.java
@@ -1,6 +1,8 @@
 package bio.terra.pipelines.app.common;
 
 import io.micrometer.core.instrument.Metrics;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 /**
  * Class used to interact with micrometer metrics that are exported to /actuator/prometheus. This
@@ -8,6 +10,7 @@ import io.micrometer.core.instrument.Metrics;
  * href="https://github.com/DataBiosphere/terra-billing-profile-manager/blob/main/service/src/main/java/bio/terra/profile/app/common/MetricUtils.java">bpm
  * metrics</a>
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MetricsUtils {
   private static final String NAMESPACE = "tsps";
   private static final String PIPELINE_TAG = "pipeline";
