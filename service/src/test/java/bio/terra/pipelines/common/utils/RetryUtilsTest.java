d4a21e950173c4d7d68da8e574b3bf203699c3a2
Morgan Taylor <marymorg@broadinstitute.org>
2023-12-04 14:03:40 -0500

    finish RetryUtils tests

--- service/src/test/java/bio/terra/pipelines/common/utils/RetryUtilsTest.java
+++ service/src/test/java/bio/terra/pipelines/common/utils/RetryUtilsTest.java
@@ -5,12 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import bio.terra.pipelines.dependencies.stairway.exception.InternalStairwayException;
+import bio.terra.pipelines.dependencies.stairway.exception.InvalidResultStateException;
 import bio.terra.pipelines.testutils.BaseContainerTest;
 import bio.terra.pipelines.testutils.StairwayTestUtils;
 import bio.terra.stairway.FlightState;
 import bio.terra.stairway.FlightStatus;
 import bio.terra.stairway.Stairway;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -67,6 +70,56 @@ class RetryUtilsTest extends BaseContainerTest {
             null);
 
     assertEquals(FlightStatus.SUCCESS, flightStateResult.getFlightStatus());
+  }
+
+  @Test
+  void getWithRetryOnException_retryableIsRetried() throws Exception {
+    // if the exception is not in the list of retryable exceptions, it should be thrown
+    List<Class<? extends Exception>> retryableExceptions = new ArrayList<>();
+    retryableExceptions.add(InvalidResultStateException.class);
+
+    InvalidResultStateException retryableException =
+        new InvalidResultStateException("test exception");
+
+    when(mockStairway.getFlightState(testFlightId))
+        .thenThrow(retryableException)
+        .thenReturn(successFlightState);
+
+    FlightState flightStateResult =
+        RetryUtils.getWithRetryOnException(
+            () -> mockStairway.getFlightState(testFlightId),
+            TEST_RETRY_TOTAL_DURATION,
+            TEST_RETRY_SLEEP_DURATION,
+            TEST_RETRY_FACTOR_INCREASE,
+            TEST_RETRY_SLEEP_DURATION_MAX,
+            retryableExceptions);
+
+    assertEquals(FlightStatus.SUCCESS, flightStateResult.getFlightStatus());
+  }
+
+  @Test
+  void getWithRetryOnException_nonRetryableThrows() throws Exception {
+    // if the exception is not in the list of retryable exceptions, it should be thrown
+    List<Class<? extends Exception>> retryableExceptions = new ArrayList<>();
+    retryableExceptions.add(InvalidResultStateException.class);
+
+    InternalStairwayException nonRetryableException =
+        new InternalStairwayException("test exception");
+
+    when(mockStairway.getFlightState(testFlightId))
+        .thenThrow(nonRetryableException)
+        .thenReturn(successFlightState);
+
+    assertThrows(
+        InternalStairwayException.class,
+        () ->
+            RetryUtils.getWithRetryOnException(
+                () -> mockStairway.getFlightState(testFlightId),
+                TEST_RETRY_TOTAL_DURATION,
+                TEST_RETRY_SLEEP_DURATION,
+                TEST_RETRY_FACTOR_INCREASE,
+                TEST_RETRY_SLEEP_DURATION_MAX,
+                retryableExceptions));
   }
 
   @Test
