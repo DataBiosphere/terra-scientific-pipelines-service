b037abd806c73be252b0b916c33ab499bde45a9a
Morgan Taylor <marymorg@broadinstitute.org>
2024-01-22 09:50:39 -0500

    clean up JobMapKeys

--- service/src/main/java/bio/terra/pipelines/dependencies/stairway/JobMapKeys.java
+++ service/src/main/java/bio/terra/pipelines/dependencies/stairway/JobMapKeys.java
@@ -6,12 +6,9 @@ public enum JobMapKeys {
   REQUEST("request"),
   USER_ID("user_id"),
   PIPELINE_ID("pipeline_id"),
-  RESPONSE("response"), // TODO what will this actually be used for?
   STATUS_CODE("status_code"),
-  RESULT_PATH("result_path"),
-
-  // parameter for the job
-  FLIGHT_CLASS("flight_class");
+  RESPONSE("response"), // used for synchronous jobs
+  RESULT_PATH("result_path"); // used for asynchronous jobs
 
   private final String keyName;
 
