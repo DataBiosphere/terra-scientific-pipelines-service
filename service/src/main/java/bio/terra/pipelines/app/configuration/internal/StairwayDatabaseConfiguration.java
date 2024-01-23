7714e8c7b2abb7fff8f9fd48b3a45b448e5d6a24
Jose Soto <jsoto@broadinstitute.org>
2024-01-23 15:57:43 -0500

    remove unused properties from StairwayDatabaseConfiguration

--- service/src/main/java/bio/terra/pipelines/app/configuration/internal/StairwayDatabaseConfiguration.java
+++ service/src/main/java/bio/terra/pipelines/app/configuration/internal/StairwayDatabaseConfiguration.java
@@ -17,38 +17,6 @@ import org.springframework.context.annotation.Configuration;
     havingValue = "false",
     matchIfMissing = true)
 public class StairwayDatabaseConfiguration extends BaseDatabaseProperties {
-  /** Passed to Stairway, true will run the migrate to upgrade the database */
-  private boolean migrateUpgrade;
-  /**
-   * Passed to Stairway, true will drop any existing stairway data and purge the work queue.
-   * Otherwise existing flights are recovered.
-   */
-  private boolean forceClean;
-
-  public boolean getMigrateUpgrade() {
-    return migrateUpgrade;
-  }
-
-  public void setMigrateUpgrade(boolean migrateUpgrade) {
-    this.migrateUpgrade = migrateUpgrade;
-  }
-
-  public boolean getForceClean() {
-    return forceClean;
-  }
-
-  public void setForceClean(boolean forceClean) {
-    this.forceClean = forceClean;
-  }
-
-  public boolean isMigrateUpgrade() {
-    return getMigrateUpgrade();
-  }
-
-  public boolean isForceClean() {
-    return getForceClean();
-  }
-
   private DataSource dataSource;
 
   public DataSource getDataSource() {
--- service/src/test/java/bio/terra/pipelines/configuration/internal/StairwayDatabaseConfigurationTest.java
+++ service/src/test/java/bio/terra/pipelines/configuration/internal/StairwayDatabaseConfigurationTest.java
@@ -3,18 +3,22 @@ package bio.terra.pipelines.configuration.internal;
 import static org.junit.jupiter.api.Assertions.*;
 
 import bio.terra.pipelines.app.configuration.internal.StairwayDatabaseConfiguration;
-import bio.terra.pipelines.testutils.BaseEmbeddedDbTest;
+import bio.terra.pipelines.testutils.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class StairwayDatabaseConfigurationTest extends BaseEmbeddedDbTest {
+@SpringBootTest(
+    properties = {
+      "spring.main.lazy-initialization=true",
+      "datasource.testWithEmbeddedDatabase=false"
+    })
+public class StairwayDatabaseConfigurationTest extends BaseTest {
 
   @Autowired StairwayDatabaseConfiguration stairwayDatabaseConfiguration;
 
   @Test
   void testStairwayDatabaseConfiguration() {
-    assertTrue(stairwayDatabaseConfiguration.getForceClean());
-    assertFalse(stairwayDatabaseConfiguration.getMigrateUpgrade());
     assertNotNull(stairwayDatabaseConfiguration.getDataSource());
   }
 }
