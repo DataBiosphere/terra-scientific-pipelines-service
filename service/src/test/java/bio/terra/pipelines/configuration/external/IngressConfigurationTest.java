ac3d2e720df7b64799b236d687f7a9ee3a9dec45
Morgan Taylor <marymorg@broadinstitute.org>
2024-02-12 11:46:58 -0500

    add IngressConfigurationTest

---
+++ service/src/test/java/bio/terra/pipelines/configuration/external/IngressConfigurationTest.java
@@ -0,0 +1,17 @@
+package bio.terra.pipelines.configuration.external;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import bio.terra.pipelines.app.configuration.external.IngressConfiguration;
+import bio.terra.pipelines.testutils.BaseEmbeddedDbTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IngressConfigurationTest extends BaseEmbeddedDbTest {
+  @Autowired IngressConfiguration ingressConfiguration;
+
+  @Test
+  void verifyIngressConfiguration() {
+    assertNotNull(ingressConfiguration.getDomainName());
+  }
+}
