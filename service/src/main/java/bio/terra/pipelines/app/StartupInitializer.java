e27fb93dfe454a7510b4931f05afc200d260a93c
Jose Soto <jsoto@broadinstitute.org>
2024-02-18 19:53:55 -0500

    hook up sentry to services to log 5** exceptions

--- service/src/main/java/bio/terra/pipelines/app/StartupInitializer.java
+++ service/src/main/java/bio/terra/pipelines/app/StartupInitializer.java
@@ -1,12 +1,17 @@
 package bio.terra.pipelines.app;
 
 import bio.terra.common.migrate.LiquibaseMigrator;
+import bio.terra.pipelines.app.configuration.external.SentryConfiguration;
 import bio.terra.pipelines.app.configuration.internal.TspsDatabaseConfiguration;
 import bio.terra.pipelines.dependencies.stairway.JobService;
+import io.sentry.Sentry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 public final class StartupInitializer {
   private static final String CHANGELOG_PATH = "db/changelog.xml";
+  private static final Logger logger = LoggerFactory.getLogger(StartupInitializer.class);
 
   public static void initialize(ApplicationContext applicationContext) {
     // Initialize the Terra Scientific Pipelines Service library
@@ -23,5 +28,18 @@ public final class StartupInitializer {
 
     // The JobService initialization also handles Stairway initialization.
     jobService.initialize();
+
+    // initialize sentry if sentry dsn env variable is available
+    SentryConfiguration sentryConfiguration = applicationContext.getBean(SentryConfiguration.class);
+    if (sentryConfiguration.dsn().isEmpty()) {
+      logger.info("No Sentry DSN found. Starting up without it.");
+    } else {
+      logger.info("Sentry DSN found. 5xx errors will be sent to Sentry.");
+      Sentry.init(
+          options -> {
+            options.setDsn(sentryConfiguration.dsn());
+            options.setEnvironment(sentryConfiguration.environment());
+          });
+    }
   }
 }
