0b193e199e0a1e28ae3ece533835ec3e19d75315
Morgan Taylor <marymorg@broadinstitute.org>
2024-04-30 13:48:05 -0400

    minor log update

--- service/src/main/java/bio/terra/pipelines/service/PipelinesService.java
+++ service/src/main/java/bio/terra/pipelines/service/PipelinesService.java
@@ -64,12 +64,12 @@ public class PipelinesService {
 
     LinkedHashMap<String, Object> inputsMap = castInputsToMap(inputs);
 
-    checkForExtraInputs(inputDefinitions, inputsMap);
-
     ArrayList<String> errorMessages =
         new ArrayList<>(validateRequiredInputs(inputDefinitions, inputsMap));
 
     errorMessages.addAll(validateInputTypes(inputDefinitions, inputsMap));
+
+    checkForExtraInputs(pipeline, inputDefinitions, inputsMap);
 
     if (!errorMessages.isEmpty()) {
       throw new ValidationException(
@@ -90,7 +90,7 @@ public class PipelinesService {
    *
    * @param inputDefinitions - list of input definitions for a pipeline
    * @param inputsMap - map of inputs to validate
-   * @return list of error messages for missing required inputs
+   * @return list of egrror messages for missing required inputs
    */
   public List<String> validateRequiredInputs(
       List<PipelineInputDefinition> inputDefinitions, Map<String, Object> inputsMap) {
@@ -146,13 +146,13 @@ public class PipelinesService {
    * unexpected inputs
    */
   public void checkForExtraInputs(
-      List<PipelineInputDefinition> inputDefinitions, Map<String, Object> inputsMap) {
+      Pipeline pipeline, List<PipelineInputDefinition> inputDefinitions, Map<String, Object> inputsMap) {
     Set<String> expectedInputNames =
         inputDefinitions.stream().map(PipelineInputDefinition::getName).collect(Collectors.toSet());
     Set<String> providedInputNames = new HashSet<>(inputsMap.keySet());
     providedInputNames.removeAll(expectedInputNames);
     if (!providedInputNames.isEmpty()) {
-      logger.warn("Extra inputs provided: {}", String.join(", ", providedInputNames));
+      logger.warn("Extra inputs provided for pipeline {}: {}", pipeline.getName(), String.join(", ", providedInputNames));
     }
   }
 }
