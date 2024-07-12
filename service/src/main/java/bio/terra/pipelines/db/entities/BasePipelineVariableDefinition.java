60f49e166ab2566f05ddc2c537706451993cb384
Morgan Taylor <marymorg@broadinstitute.org>
2024-07-12 12:42:41 -0400

    add base pipeline variable definition class

---
+++ service/src/main/java/bio/terra/pipelines/db/entities/BasePipelineVariableDefinition.java
@@ -0,0 +1,36 @@
+package bio.terra.pipelines.db.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BasePipelineVariableDefinition {
+  @Id
+  @Column(name = "id", nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "pipeline_id")
+  private Long pipelineId;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "wdl_variable_name", nullable = false)
+  private String wdlVariableName;
+
+  protected BasePipelineVariableDefinition(Long pipelineId, String name, String wdlVariableName) {
+    this.pipelineId = pipelineId;
+    this.name = name;
+    this.wdlVariableName = wdlVariableName;
+  }
+}
--- service/src/main/java/bio/terra/pipelines/db/entities/PipelineOutputDefinition.java
+++ service/src/main/java/bio/terra/pipelines/db/entities/PipelineOutputDefinition.java
@@ -2,9 +2,6 @@ package bio.terra.pipelines.db.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,29 +12,14 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Table(name = "pipeline_output_definitions")
-public class PipelineOutputDefinition {
-  @Id
-  @Column(name = "id", nullable = false)
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-
-  @Column(name = "pipeline_id")
-  private Long pipelineId;
-
-  @Column(name = "name", nullable = false)
-  private String name;
-
-  @Column(name = "wdl_variable_name", nullable = false)
-  private String wdlVariableName;
+public class PipelineOutputDefinition extends BasePipelineVariableDefinition {
 
   @Column(name = "type", nullable = false)
   private String type;
 
   public PipelineOutputDefinition(
       Long pipelineId, String name, String wdlVariableName, String type) {
-    this.pipelineId = pipelineId;
-    this.name = name;
-    this.wdlVariableName = wdlVariableName;
+    super(pipelineId, name, wdlVariableName);
     this.type = type;
   }
 }
