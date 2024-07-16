28afa8798346a051791f67912302d0f45d7c5186
Morgan Taylor <marymorg@broadinstitute.org>
2024-07-16 16:07:01 -0400

    rename PipelineInputTypeConverter to PipelineVariableTypeConverter

--- service/src/main/java/bio/terra/pipelines/db/entities/converters/PipelineVariableTypeConverter.java
+++ service/src/main/java/bio/terra/pipelines/db/entities/converters/PipelineVariableTypeConverter.java
@@ -6,15 +6,15 @@ import jakarta.persistence.Converter;
 
 // inspired by https://www.baeldung.com/jpa-persisting-enums-in-jpa
 @Converter(autoApply = true)
-public class PipelineInputTypeConverter
+public class PipelineVariableTypeConverter
     implements AttributeConverter<PipelineVariableTypesEnum, String> {
   @Override
-  public String convertToDatabaseColumn(PipelineVariableTypesEnum pipelineInputTypeEnum) {
-    return pipelineInputTypeEnum.toString();
+  public String convertToDatabaseColumn(PipelineVariableTypesEnum pipelineVariableTypeEnum) {
+    return pipelineVariableTypeEnum.toString();
   }
 
   @Override
-  public PipelineVariableTypesEnum convertToEntityAttribute(String pipelineInputTypeString) {
-    return PipelineVariableTypesEnum.valueOf(pipelineInputTypeString);
+  public PipelineVariableTypesEnum convertToEntityAttribute(String pipelineVariableTypeString) {
+    return PipelineVariableTypesEnum.valueOf(pipelineVariableTypeString);
   }
 }
