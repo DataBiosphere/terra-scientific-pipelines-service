5fed85fd5ff2339b19625a03c7bd79af7e47508a
jsotobroad <jsoto@broadinstitute.com>
2024-08-02 12:42:33 -0400

    TSPS-274 add rawls client to codebase (#108)
    Co-authored-by: Jose Soto <jsoto@broadinstitute.org>

---
+++ service/src/test/java/bio/terra/pipelines/dependencies/rawls/RawlsClientTest.java
@@ -0,0 +1,48 @@
+package bio.terra.pipelines.dependencies.rawls;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import bio.terra.pipelines.testutils.BaseEmbeddedDbTest;
+import bio.terra.rawls.api.EntitiesApi;
+import bio.terra.rawls.api.StatusApi;
+import bio.terra.rawls.client.ApiClient;
+import bio.terra.rawls.client.auth.Authentication;
+import bio.terra.rawls.client.auth.OAuth;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class RawlsClientTest extends BaseEmbeddedDbTest {
+  @Autowired RawlsClient rawlsClient;
+
+  String rawlsBaseUri = "https://test_rawls_url/";
+  String authToken = "authToken";
+
+  @Test
+  void testWorkspaceAuthorizedClient() {
+    ApiClient apiClient = rawlsClient.getUnauthorizedApiClient();
+
+    assertEquals(rawlsBaseUri, apiClient.getBasePath());
+    assertTrue(apiClient.isDebugging());
+
+    apiClient = rawlsClient.getApiClient(authToken);
+    for (Authentication auth : apiClient.getAuthentications().values()) {
+      if (auth instanceof OAuth) {
+        String actualToken = ((OAuth) auth).getAccessToken();
+        assertEquals(authToken, actualToken);
+        return; // rawls client only adds a token to the first instance in the list
+      }
+    }
+  }
+
+  @Test
+  void testWorkspaceClientApis() {
+    StatusApi unauthenticatedApi = rawlsClient.getStatusApi();
+    assertEquals(rawlsBaseUri, unauthenticatedApi.getApiClient().getBasePath());
+    assertTrue(unauthenticatedApi.getApiClient().isDebugging());
+
+    EntitiesApi resourceApi = rawlsClient.getEntitiesApi(authToken);
+    assertEquals(rawlsBaseUri, resourceApi.getApiClient().getBasePath());
+    assertTrue(resourceApi.getApiClient().isDebugging());
+  }
+}
