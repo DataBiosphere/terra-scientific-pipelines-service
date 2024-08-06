390eccb98b22fa285bc9c50acf3439e643f1fe1c
Jose Soto <jsoto@broadinstitute.org>
2024-08-06 11:02:19 -0400

    same as last commit but actually correctly

--- service/src/main/java/bio/terra/pipelines/dependencies/rawls/RawlsServiceApiException.java
+++ service/src/main/java/bio/terra/pipelines/dependencies/rawls/RawlsServiceApiException.java
@@ -2,6 +2,7 @@ package bio.terra.pipelines.dependencies.rawls;
 
 import bio.terra.rawls.client.ApiException;
 
+@SuppressWarnings("java:S110") // Disable "Inheritance tree of classes should not be too deep"
 public class RawlsServiceApiException extends RawlsServiceException {
 
   public RawlsServiceApiException(ApiException exception) {
--- service/src/main/java/bio/terra/pipelines/dependencies/workspacemanager/WorkspaceManagerServiceException.java
+++ service/src/main/java/bio/terra/pipelines/dependencies/workspacemanager/WorkspaceManagerServiceException.java
@@ -4,7 +4,6 @@ import bio.terra.common.exception.ErrorReportException;
 import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 
-@SuppressWarnings("java:S110") // Disable "Inheritance tree of classes should not be too deep"
 public abstract class WorkspaceManagerServiceException extends ErrorReportException {
   protected WorkspaceManagerServiceException(String message, Throwable cause) {
     super(message, cause, new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
