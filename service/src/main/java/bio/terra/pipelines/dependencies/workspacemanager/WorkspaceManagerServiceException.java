6c7205daea0ea2fed353cbc297e114021d6de850
Jose Soto <jsoto@broadinstitute.org>
2024-08-06 09:51:50 -0400

    suppress too much inheritance sonar issue java:S110

--- service/src/main/java/bio/terra/pipelines/dependencies/workspacemanager/WorkspaceManagerServiceException.java
+++ service/src/main/java/bio/terra/pipelines/dependencies/workspacemanager/WorkspaceManagerServiceException.java
@@ -4,6 +4,7 @@ import bio.terra.common.exception.ErrorReportException;
 import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 
+@SuppressWarnings("java:S110") // Disable "Inheritance tree of classes should not be too deep"
 public abstract class WorkspaceManagerServiceException extends ErrorReportException {
   protected WorkspaceManagerServiceException(String message, Throwable cause) {
     super(message, cause, new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
