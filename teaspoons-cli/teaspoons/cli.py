557dcec4d417d26e58d13bbde827bb9025b15a42
Morgan Taylor <marymorg@broadinstitute.org>
2024-10-15 12:15:53 -0400

    clean up, update README

--- teaspoons-cli/teaspoons/cli.py
+++ teaspoons-cli/teaspoons/cli.py
@@ -2,11 +2,11 @@ import typer
 from commands.auth import auth_app
 from commands.pipelines import pipelines_app
 
-app = typer.Typer()
-app.add_typer(auth_app, name="auth", help="Authentication commands")
-app.add_typer(pipelines_app, name="pipelines", help="Pipeline commands")
+cli = typer.Typer()
+cli.add_typer(auth_app, name="auth", help="Authentication commands")
+cli.add_typer(pipelines_app, name="pipelines", help="Pipeline commands")
 # will add runs_app later
 
 
 if __name__ == '__main__':
-    app()
+    cli()
