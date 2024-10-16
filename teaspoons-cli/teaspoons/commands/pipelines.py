e7afb6f612414bc3f5e68a29e1d0c05bc3ff3977
Morgan Taylor <marymorg@broadinstitute.org>
2024-10-16 15:37:27 -0400

    awesome exception handling

--- teaspoons-cli/teaspoons/commands/pipelines.py
+++ teaspoons-cli/teaspoons/commands/pipelines.py
@@ -1,8 +1,11 @@
+# pipelines.py
+
 import click
 import typer
 
 # import generated library
-from generated.teaspoons_client import PipelinesApi
+from teaspoons_client import PipelinesApi
+from utils import handle_exceptions
 
 # teaspoons modules
 from client import ClientWrapper
@@ -12,27 +15,19 @@ pipelines_app = typer.Typer()
 
 
 @pipelines_app.command()
+@handle_exceptions
 def list():
-    try:
-        with ClientWrapper() as api_client:
-            # NOTE: this is broken until this is merged with the different api def for the get_pipelines_result
-            pipeline_client = PipelinesApi(api_client=api_client)
-            pipelines = pipeline_client.get_pipelines()
-            for pipeline in pipelines.pipelines.results:
-                click.echo(f'{pipeline.name} - {pipeline.description}')
-    except ValueError as e:
-        click.echo(str(e), err=True)
-        exit(1)
-
+    with ClientWrapper() as api_client:
+        pipeline_client = PipelinesApi(api_client=api_client)
+        pipelines = pipeline_client.get_pipelines()
+        for pipeline in pipelines.pipelines.results:
+            click.echo(f'{pipeline.name} - {pipeline.description}')
 
 @pipelines_app.command()
 @click.argument('name')
+@handle_exceptions
 def get_info(name: str):
-    try:
-        with ClientWrapper() as api_client:
-            pipeline_client = PipelinesApi(api_client=api_client)
-            pipeline = pipeline_client.get_pipeline_details(pipeline_name=name)
-            _pretty_print(pipeline)
-    except ValueError as e:
-        click.echo(str(e), err=True)
-        exit(1)
+    with ClientWrapper() as api_client:
+        pipeline_client = PipelinesApi(api_client=api_client)
+        pipeline = pipeline_client.get_pipeline_details(pipeline_name=name)
+        _pretty_print(pipeline)
--- teaspoons-cli/teaspoons/utils.py
+++ teaspoons-cli/teaspoons/utils.py
@@ -3,7 +3,10 @@
 import click
 import json
 
+from functools import wraps
 from pydantic import BaseModel
+
+from teaspoons_client.exceptions import ApiException
 
 
 def _pretty_print(obj: BaseModel):
@@ -11,3 +14,18 @@ def _pretty_print(obj: BaseModel):
     Prints a pydantic model in a pretty format to the console
     """
     click.echo(json.dumps(obj.model_dump(), indent=4))
+
+
+def handle_exceptions(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ApiException as e:
+            formatted_message = f"API call failed with status code {e.status} ({e.reason}): {json.loads(e.body)['message']}"
+            click.echo(formatted_message, err=True)
+            exit(1)
+        except ValueError as e:
+            click.echo(str(e), err=True)
+            exit(1)
+    return wrapper
