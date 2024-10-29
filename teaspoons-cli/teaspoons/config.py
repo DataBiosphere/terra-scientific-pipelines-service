409a1d9e2d47dd47a96c8165c7f47a06cc9f9484
Morgan Taylor <marymorg@broadinstitute.org>
2024-10-29 13:39:11 -0400

    rename .env to .teaspoons-cli-config

--- teaspoons-cli/teaspoons/config.py
+++ teaspoons-cli/teaspoons/config.py
@@ -8,20 +8,8 @@ from dotenv import dotenv_values
 class CliConfig:
     """A class to hold configuration information for the CLI"""
 
-    # def __init__(self, env_file: str = "../.env"):
-    #     self.config = dotenv_values(env_file)
-
-    #     if 'tests' not in env_file:
-    #         self.client_info = OAuth2ClientInfo.from_oidc_endpoint(
-    #             self.config["OAUTH_OPENID_CONFIGURATION_URI"],
-    #             client_id=self.config["OAUTH_CLIENT_ID"],
-    #             scopes=[f"openid+email+profile+{self.config['OAUTH_CLIENT_ID']}"]
-    #         )
-    #     else:
-    #         self.client_info = None
-
     def __init__(self):
-        self.config = dotenv_values("../.env")
+        self.config = dotenv_values("../.teaspoons-cli-config")
 
         self.client_info = OAuth2ClientInfo.from_oidc_endpoint(
             self.config["OAUTH_OPENID_CONFIGURATION_URI"],
@@ -34,10 +22,3 @@ class CliConfig:
         self.token_file = (
             f'{Path.home()}/{self.config["LOCAL_STORAGE_PATH"]}/access_token'
         )
-
-
-# def get_cli_config() -> CliConfig:
-#     if 'pytest' not in sys.modules:
-#         return CliConfig()
-#     else:
-#         return CliConfig(env_file='tests/.env.test')
