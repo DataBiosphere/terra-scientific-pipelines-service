b22bbb9d9c74c36e78b39f8ac48d07aab660aa1b
Morgan Taylor <marymorg@broadinstitute.org>
2024-10-31 13:38:30 -0400

    delete teaspoons-cli dir and contents

--- teaspoons-cli/teaspoons/log.py
+++
@@ -1,8 +0,0 @@
-# log.py
-
-import logging
-
-
-def configure_logging(debug: bool):
-    log_level = logging.DEBUG if debug else logging.INFO
-    logging.basicConfig(level=log_level, format="%(message)s")
