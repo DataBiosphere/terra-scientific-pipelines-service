2cc8b1c1d3dcdad7403c0af297e1c39f008e2e1c
Jose Soto <jsoto@broadinstitute.org>
2024-11-15 12:15:05 -0500

    add quotas controller with api to get quota for a pipeline

---
+++ service/src/main/java/bio/terra/pipelines/app/controller/QuotasController.java
@@ -0,0 +1,64 @@
+package bio.terra.pipelines.app.controller;
+
+import bio.terra.common.iam.SamUser;
+import bio.terra.common.iam.SamUserFactory;
+import bio.terra.pipelines.app.configuration.external.SamConfiguration;
+import bio.terra.pipelines.common.utils.PipelinesEnum;
+import bio.terra.pipelines.db.entities.UserQuota;
+import bio.terra.pipelines.generated.api.QuotasApi;
+import bio.terra.pipelines.generated.model.ApiQuotaWithDetails;
+import bio.terra.pipelines.service.QuotasService;
+import io.swagger.annotations.Api;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+/** Quotas controller */
+@Controller
+@Api(tags = {"quotas"})
+public class QuotasController implements QuotasApi {
+  private final SamConfiguration samConfiguration;
+  private final SamUserFactory samUserFactory;
+  private final HttpServletRequest request;
+  private final QuotasService quotasService;
+
+  private final Logger logger = LoggerFactory.getLogger(QuotasController.class);
+
+  private SamUser getAuthenticatedInfo() {
+    return samUserFactory.from(request, samConfiguration.baseUri());
+  }
+
+  QuotasController(
+      SamConfiguration samConfiguration,
+      SamUserFactory samUserFactory,
+      HttpServletRequest request,
+      QuotasService quotasService) {
+    this.samConfiguration = samConfiguration;
+    this.samUserFactory = samUserFactory;
+    this.request = request;
+    this.quotasService = quotasService;
+  }
+
+  /** Returns the quota usage for the authenticated user for the specified pipeline. */
+  @Override
+  public ResponseEntity<ApiQuotaWithDetails> getQuotaForPipeline(String pipelineName) {
+    SamUser user = getAuthenticatedInfo();
+    PipelinesEnum validatedPipelineName =
+        PipelineApiUtils.validatePipelineName(pipelineName, logger);
+    UserQuota userQuota =
+        quotasService.getQuotaForUserAndPipeline(user.getSubjectId(), validatedPipelineName);
+
+    return new ResponseEntity<>(quotasToApiQuotaWithDetails(userQuota), HttpStatus.OK);
+  }
+
+  static ApiQuotaWithDetails quotasToApiQuotaWithDetails(UserQuota userQuota) {
+
+    return new ApiQuotaWithDetails()
+        .pipelineName(userQuota.getPipelineName().getValue())
+        .quotaConsumed(userQuota.getQuotaConsumed())
+        .quotaLimit(userQuota.getQuota());
+  }
+}
---
+++ service/src/test/java/bio/terra/pipelines/controller/QuotasControllerTest.java
@@ -0,0 +1,87 @@
+package bio.terra.pipelines.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import bio.terra.common.iam.SamUser;
+import bio.terra.common.iam.SamUserFactory;
+import bio.terra.pipelines.app.configuration.external.SamConfiguration;
+import bio.terra.pipelines.app.controller.GlobalExceptionHandler;
+import bio.terra.pipelines.app.controller.QuotasController;
+import bio.terra.pipelines.common.utils.PipelinesEnum;
+import bio.terra.pipelines.db.entities.UserQuota;
+import bio.terra.pipelines.db.exception.InvalidPipelineException;
+import bio.terra.pipelines.generated.model.ApiQuotaWithDetails;
+import bio.terra.pipelines.service.QuotasService;
+import bio.terra.pipelines.testutils.MockMvcUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@ContextConfiguration(classes = {QuotasController.class, GlobalExceptionHandler.class})
+@WebMvcTest
+public class QuotasControllerTest {
+  @MockBean QuotasService quotasServiceMock;
+  @MockBean SamUserFactory samUserFactoryMock;
+  @MockBean SamConfiguration samConfiguration;
+
+  @Autowired private MockMvc mockMvc;
+
+  private final SamUser testUser = MockMvcUtils.TEST_SAM_USER;
+  private final UserQuota testUserQuota = MockMvcUtils.TEST_USER_QUOTA_1;
+
+  @BeforeEach
+  void beforeEach() {
+    when(samConfiguration.baseUri()).thenReturn("baseSamUri");
+    when(samUserFactoryMock.from(any(HttpServletRequest.class), eq("baseSamUri")))
+        .thenReturn(testUser);
+    when(quotasServiceMock.getQuotaForUserAndPipeline(
+            testUser.getSubjectId(), PipelinesEnum.ARRAY_IMPUTATION))
+        .thenReturn(testUserQuota);
+  }
+
+  @Test
+  void getQuotaOk() throws Exception {
+
+    MvcResult result =
+        mockMvc
+            .perform(get("/api/quotas/v1/" + PipelinesEnum.ARRAY_IMPUTATION.getValue()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+    ApiQuotaWithDetails response =
+        new ObjectMapper()
+            .readValue(result.getResponse().getContentAsString(), ApiQuotaWithDetails.class);
+
+    assertEquals(testUserQuota.getQuota(), response.getQuotaLimit());
+    assertEquals(testUserQuota.getQuotaConsumed(), response.getQuotaConsumed());
+    assertEquals(testUserQuota.getPipelineName().getValue(), response.getPipelineName());
+  }
+
+  @Test
+  void getQuotasBadPipeline() throws Exception {
+    String pipelineName = "bad-pipeline-id";
+
+    mockMvc
+        .perform(get("/api/quotas/v1/" + pipelineName))
+        .andExpect(status().isBadRequest())
+        .andExpect(
+            result ->
+                assertInstanceOf(InvalidPipelineException.class, result.getResolvedException()));
+  }
+}
