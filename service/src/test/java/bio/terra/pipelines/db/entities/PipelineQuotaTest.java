cb760d0170325210c6a2c4dd9456fe92c29c83b6
Jose Soto <jsoto@broadinstitute.org>
2024-12-12 18:26:58 -0500

    add tests for coverage

---
+++ service/src/test/java/bio/terra/pipelines/db/entities/PipelineQuotaTest.java
@@ -0,0 +1,18 @@
+package bio.terra.pipelines.db.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bio.terra.pipelines.common.utils.PipelinesEnum;
+import bio.terra.pipelines.testutils.BaseTest;
+import org.junit.jupiter.api.Test;
+
+class PipelineQuotaTest extends BaseTest {
+
+  @Test
+  void testPipelineQuotaConstructor() {
+    PipelineQuota pipelineQuota = new PipelineQuota(PipelinesEnum.ARRAY_IMPUTATION, 10000, 500);
+    assertEquals(PipelinesEnum.ARRAY_IMPUTATION, pipelineQuota.getPipelineName());
+    assertEquals(10000, pipelineQuota.getDefaultQuota());
+    assertEquals(500, pipelineQuota.getMinQuotaConsumed());
+  }
+}
