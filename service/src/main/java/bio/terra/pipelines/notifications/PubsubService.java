ae3ea1b954dd3ad85d5f3682884d372681d84355
Morgan Taylor <marymorg@broadinstitute.org>
2024-12-17 09:42:55 -0500

    shut down pubsub publisher after sending message

--- service/src/main/java/bio/terra/pipelines/notifications/PubsubService.java
+++ service/src/main/java/bio/terra/pipelines/notifications/PubsubService.java
@@ -1,12 +1,18 @@
 package bio.terra.pipelines.notifications;
 
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.rpc.ApiException;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.cloud.pubsub.v1.TopicAdminClient;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.Topic;
 import com.google.pubsub.v1.TopicName;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -28,9 +34,59 @@ public class PubsubService {
     }
   }
 
-  public void publishMessage(String projectId, String topicId, String message) throws IOException {
+  public void publishMessage(String projectId, String topicId, String message)
+      throws IOException, InterruptedException {
+    //    TopicName topicName = TopicName.of(projectId, topicId);
+    //    var publisher = Publisher.newBuilder(topicName).build();
+    //
+    // publisher.publish(PubsubMessage.newBuilder().setData(ByteString.copyFromUtf8(message)).build());
+    //    publisher.awaitTermination()
+
     TopicName topicName = TopicName.of(projectId, topicId);
-    var publisher = Publisher.newBuilder(topicName).build();
-    publisher.publish(PubsubMessage.newBuilder().setData(ByteString.copyFromUtf8(message)).build());
+    Publisher publisher = null;
+
+    try {
+      // Create a publisher instance with default settings bound to the topic
+      publisher = Publisher.newBuilder(topicName).build();
+
+      ByteString data = ByteString.copyFromUtf8(message);
+      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+      // Once published, returns a server-assigned message id (unique within the topic)
+      ApiFuture<String> future = publisher.publish(pubsubMessage);
+
+      // Add an asynchronous callback to handle success / failure
+      ApiFutures.addCallback(
+          future,
+          new ApiFutureCallback<String>() {
+
+            @Override
+            public void onFailure(Throwable throwable) {
+              if (throwable instanceof ApiException) {
+                ApiException apiException = ((ApiException) throwable);
+                // details on the API exception
+                logger.warn(
+                    "Google API exception! status code: {}, is retryable: {}",
+                    apiException.getStatusCode().getCode(),
+                    apiException.isRetryable());
+              }
+              logger.error("Error publishing message to Google PubSub: {}", message);
+            }
+
+            @Override
+            public void onSuccess(String messageId) {
+              // Once published, returns server-assigned message ids (unique within the topic)
+              logger.info("Published message ID: {}", messageId);
+            }
+          },
+          MoreExecutors.directExecutor());
+
+    } finally {
+      if (publisher != null) {
+        // When finished with the publisher, shutdown to free up resources.
+        publisher.shutdown();
+        publisher.awaitTermination(1, TimeUnit.MINUTES);
+      }
+    }
   }
 }
