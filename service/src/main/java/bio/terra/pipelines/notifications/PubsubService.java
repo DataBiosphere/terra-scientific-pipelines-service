eff5b8affbf5808ac02c36b7a225524fb0ff031e
Morgan Taylor <marymorg@broadinstitute.org>
2024-12-19 10:34:51 -0500

    update db changelog, try creating publisher once

--- service/src/main/java/bio/terra/pipelines/notifications/PubsubService.java
+++ service/src/main/java/bio/terra/pipelines/notifications/PubsubService.java
@@ -10,7 +10,6 @@ import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -20,54 +19,77 @@ import org.springframework.stereotype.Service;
 public class PubsubService {
   private static final Logger logger = LoggerFactory.getLogger(PubsubService.class);
 
+  private Publisher publisher;
+
+  private void createOrGetPublisher(TopicName topicName) throws IOException {
+    if (this.publisher != null) {
+      logger.info("Publisher already exists for Google PubSub topicName {}", topicName);
+      return;
+    }
+    try {
+      logger.info("Creating publisher for Google PubSub topicName {}", topicName);
+      // Create a publisher instance with default settings bound to the topic
+      this.publisher = Publisher.newBuilder(topicName).build();
+    } catch (IOException e) {
+      logger.error("Error creating publisher for Google PubSub topicName {}", topicName);
+      throw e;
+    }
+  }
+
   /** Publish a message to a Google PubSub topic. */
   public void publishMessage(String projectId, String topicId, String message)
       throws IOException, InterruptedException {
     TopicName topicName = TopicName.of(projectId, topicId);
-    Publisher publisher = null;
-    logger.info("Publishing message to Google PubSub projectId {}, topicId {}", projectId, topicId);
+    logger.info(
+        "Publishing message to Google PubSub projectId {}, topicId {}: {}",
+        projectId,
+        topicId,
+        message);
 
-    try {
-      // Create a publisher instance with default settings bound to the topic
-      publisher = Publisher.newBuilder(topicName).build();
+    //    try {
+    createOrGetPublisher(topicName);
 
-      ByteString data = ByteString.copyFromUtf8(message);
-      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+    ByteString data = ByteString.copyFromUtf8(message);
+    PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
 
-      // Once published, returns a server-assigned message id (unique within the topic)
-      ApiFuture<String> future = publisher.publish(pubsubMessage);
+    // Once published, returns a server-assigned message id (unique within the topic)
+    ApiFuture<String> future = this.publisher.publish(pubsubMessage);
 
-      // Add an asynchronous callback to handle success / failure
-      ApiFutures.addCallback(
-          future,
-          new ApiFutureCallback<String>() {
+    // Add an asynchronous callback to handle success / failure
+    ApiFutures.addCallback(
+        future,
+        new ApiFutureCallback<String>() {
 
-            @Override
-            public void onFailure(Throwable throwable) {
-              if (throwable instanceof ApiException apiException) {
-                // details on the API exception
-                logger.warn(
-                    "Google API exception: status code {}, is retryable: {}",
-                    apiException.getStatusCode().getCode(),
-                    apiException.isRetryable());
-              }
-              logger.error("Error publishing message to Google PubSub: {}", message);
+          @Override
+          public void onFailure(Throwable throwable) {
+            String errorMessage = "";
+            if (throwable instanceof ApiException apiException) {
+              // details on the API exception
+              errorMessage =
+                  "Google API exception: status code %s, is retryable: %s"
+                      .formatted(
+                          apiException.getStatusCode().getCode(), apiException.isRetryable());
             }
+            logger.error(
+                "Error publishing message to Google PubSub: {}; {}",
+                errorMessage,
+                throwable.getMessage());
+          }
 
-            @Override
-            public void onSuccess(String messageId) {
-              // Once published, returns server-assigned message ids (unique within the topic)
-              logger.info("Published message ID: {}", messageId);
-            }
-          },
-          MoreExecutors.directExecutor());
+          @Override
+          public void onSuccess(String messageId) {
+            // Once published, returns server-assigned message ids (unique within the topic)
+            logger.info("Published message ID: {}", messageId);
+          }
+        },
+        MoreExecutors.directExecutor());
 
-    } finally {
-      if (publisher != null) {
-        // When finished with the publisher, shutdown to free up resources.
-        publisher.shutdown();
-        publisher.awaitTermination(1, TimeUnit.MINUTES);
-      }
-    }
+    //    } finally {
+    //      if (publisher != null) {
+    //        // When finished with the publisher, shutdown to free up resources.
+    //        publisher.shutdown();
+    //        publisher.awaitTermination(1, TimeUnit.MINUTES);
+    //      }
   }
+  //  }
 }
