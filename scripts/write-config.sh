aa3c276bf246451ce60cfb9bdffb37264a43790b
Jose Soto <jsoto@broadinstitute.org>
2025-01-23 12:18:14 -0500

    clean up write-config script

--- scripts/write-config.sh
+++ scripts/write-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# write-config.sh extracts configuration information from vault and writes it to a set of files
+# write-config.sh extracts configuration information from GSM and writes it to a set of files
 # in a directory. This simplifies access to the secrets from other scripts and applications.
 #
 # We want to use this in a gradle task, so it takes arguments both as command line
@@ -13,56 +13,29 @@
 # For personal environments, we assume that the target name is the same as the personal namespace name.
 # The output directory includes the following files:
 #   ---------------------------+-------------------------------------------------------------------------
-#   db-connection-name.txt     | Connection string for CloudSQL proxy
-#   ---------------------------+-------------------------------------------------------------------------
-#   db-name.txt                | Database name
-#   ---------------------------+-------------------------------------------------------------------------
-#   db-password.txt            | Database password
-#   ---------------------------+-------------------------------------------------------------------------
-#   db-username.txt            | Database username
-#   ---------------------------+-------------------------------------------------------------------------
-#   sqlproxy-sa.json           | SA of the CloudSQL proxy
-#   ---------------------------+-------------------------------------------------------------------------
 #   target.txt                 | the target that generated this set of config files. Allows the script
 #                              | to skip regenerating the environment on a rerun.
 #   ---------------------------+-------------------------------------------------------------------------
-#   testrunner-sa.json         | SA for running TestRunner - this is always taken from integration/common
-#   ---------------------------+-------------------------------------------------------------------------
-#   testrunner-k8s-sa-token.txt| Credentials for TestRunner to manipulate the Kubernetes cluster under
-#   testrunner-k8s-sa-key.txt  | test. Not all environments have this SA configured. If the k8env is 
-#                              | integration and there is no configured SA, then the wsmtest one will be
-#                              | retrieved. It won't work for all test runner tests.
-#   ---------------------------+-------------------------------------------------------------------------
-#   user-delegated-sa.json     | Firecloud SA used to masquerade as test users
-#   ---------------------------+-------------------------------------------------------------------------
-#   policy-sa.json             | SA that the policy service runs as
+#   teaspoons-sa.json         | SA for running Teaspoons - this is taken from GSM for the provided target
 #   ---------------------------+-------------------------------------------------------------------------
 
 function usage {
   cat <<EOF
-Usage: $0 [<target>] [<vaulttoken>] [<outputdir>] [<vaultenv>]"
+Usage: $0 [<target>] [<outputdir>] "
 
   <target> can be:
     local - for testing against a local server (bootRun)
     dev - uses secrets from the dev environment
-    alpha - alpha test environment
-    staging - release staging environment
+    qa - uses secrets from the qa environment
     help or ? - print this help
     clean - removes all files from the output directory
     * - anything else is assumed to be a personal environment using the terra-kernel-k8s
   If <target> is not specified, then use the envvar TEASPOONS_WRITE_CONFIG
   If TEASPOONS_WRITE_CONFIG is not specified, then use local
 
-  <vaulttoken> defaults to the token found in ~/.vault-token.
-
   <outputdir> defaults to "../config/" relative to the script. When run from the gradle rootdir, it will be
   in the expected place for automation.
 
-  <vaultenv> can be:
-    docker - run a docker image containing vault
-    local  - run the vault installed locally
-  If <vaultenv> is not specified, then use the envvar TEASPOONS_VAULT_ENV
-  If TEASPOONS_VAULT_ENV is not specified, then we use docker
 EOF
  exit 1
 }
@@ -72,13 +45,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null  && pwd )"
 default_outputdir="${script_dir}/config"
 default_target=${TEASPOONS_WRITE_CONFIG:-local}
 target=${1:-$default_target}
-vaulttoken=${2:-$(cat "$HOME"/.vault-token)}
-outputdir=${3:-$default_outputdir}
-default_vaultenv=${TEASPOONS_VAULT_ENV:-docker}
-vaultenv=${4:-$default_vaultenv}
-
-# The vault paths are irregular, so we map the target into separate variables:
-# fcenv     - the firecloud delegated service account environment: dev, qa
+outputdir=${2:-$default_outputdir}
 
 case $target in
     help | ?)
@@ -123,62 +90,6 @@ if [ -e "${outputdir}/target.txt" ]; then
         exit 0
     fi
 fi
-
-# Run vault either using a docker container or using the installed vault
-function dovault {
-    local dovaultpath=$1
-    local dofilename=$2
-    case $vaultenv in
-        docker)
-            docker run --rm -e VAULT_TOKEN="${vaulttoken}" broadinstitute/dsde-toolbox:dev \
-                   vault read -format=json "${dovaultpath}" > "${dofilename}"
-            ;;
-
-        local)
-            VAULT_TOKEN="${vaulttoken}" VAULT_ADDR="https://clotho.broadinstitute.org:8200" \
-                   vault read -format=json "${dovaultpath}" > "${dofilename}"
-            ;;
-    esac
-}
-
-# Read a vault path into an output file, decoding from base64
-# To detect missing tokens, we need to capture the docker result before
-# doing the rest of the pipeline.
-function vaultgetb64 {
-    vaultpath=$1
-    filename=$2
-    fntmpfile=$(mktemp)
-    dovault "${vaultpath}" "${fntmpfile}"
-    result=$?
-    if [ $result -ne 0 ]; then return $result; fi
-    jq -r .data.key "${fntmpfile}" | base64 -d > "${filename}"
-}
-
-# Read a vault path into an output file
-function vaultget {
-    vaultpath=$1
-    filename=$2
-    fntmpfile=$(mktemp)
-    dovault "${vaultpath}" "${fntmpfile}"
-    result=$?
-    if [ $result -ne 0 ]; then return $result; fi
-    jq -r .data "${fntmpfile}" > "${filename}"
-}
-
-# Read database data from a vault path into a set of files
-function vaultgetdb {
-    vaultpath=$1
-    fileprefix=$2
-    fntmpfile=$(mktemp)
-    dovault "${vaultpath}" "${fntmpfile}"
-    result=$?
-    if [ $result -ne 0 ]; then return $result; fi
-    datafile=$(mktemp)
-    jq -r .data "${fntmpfile}" > "${datafile}"
-    jq -r '.db' "${datafile}" > "${outputdir}/${fileprefix}-name.txt"
-    jq -r '.password' "${datafile}" > "${outputdir}/${fileprefix}-password.txt"
-    jq -r '.username' "${datafile}" > "${outputdir}/${fileprefix}-username.txt"
-}
 
 # read a secret out of google secrets manager
 read_secret_gsm() {
