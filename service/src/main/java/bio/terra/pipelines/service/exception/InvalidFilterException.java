88e45c817dd0da479ea26b2740ea8abf2a3c0e5a
Matt Bemis <mbemis@broadinstitute.org>
2025-11-12 15:13:35 -0500

    suppress code warning, following the lead of other exceptions

--- service/src/main/java/bio/terra/pipelines/service/exception/InvalidFilterException.java
+++ service/src/main/java/bio/terra/pipelines/service/exception/InvalidFilterException.java
@@ -2,6 +2,7 @@ package bio.terra.pipelines.service.exception;
 
 import bio.terra.common.exception.BadRequestException;
 
+@SuppressWarnings("java:S110") // Disable "Inheritance tree of classes should not be too deep"
 public class InvalidFilterException extends BadRequestException {
   public InvalidFilterException(String message) {
     super(message);
