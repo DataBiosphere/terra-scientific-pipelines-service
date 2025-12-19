3928fbf47e5d30529d632d3274c23284d85d4164
Saloni Shah <salonipshah11@gmail.com>
2025-12-19 17:33:53 -0500

    update readme, tests

--- service/src/main/java/bio/terra/pipelines/service/exception/PipelineInternalServerException.java
+++ service/src/main/java/bio/terra/pipelines/service/exception/PipelineInternalServerException.java
@@ -2,6 +2,7 @@ package bio.terra.pipelines.service.exception;
 
 import bio.terra.common.exception.InternalServerErrorException;
 
+@SuppressWarnings("java:S110") // Disable "Inheritance tree of classes should not be too deep"
 public class PipelineInternalServerException extends InternalServerErrorException {
   public PipelineInternalServerException() {
     super("Something went wrong while running the job. Please reach out to support for help.");
