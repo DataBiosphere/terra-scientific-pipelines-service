6ef7f71c86e6877303ccd234d2321c1edd525489
Jose Soto <jsoto@broadinstitute.org>
2026-04-23 13:35:44 -0400

    add deserializer function so internal step exceptions dont get wrapped twice before being returned to the user

--- service/src/main/java/bio/terra/pipelines/stairway/steps/exception/InternalStepException.java
+++ service/src/main/java/bio/terra/pipelines/stairway/steps/exception/InternalStepException.java
@@ -1,6 +1,7 @@
 package bio.terra.pipelines.stairway.steps.exception;
 
 import bio.terra.common.exception.InternalServerErrorException;
+import java.util.List;
 
 @SuppressWarnings("java:S110") // Disable "Inheritance tree of classes should not be too deep"
 public class InternalStepException extends InternalServerErrorException {
@@ -9,5 +10,14 @@ public class InternalStepException extends InternalServerErrorException {
     super(
         "An error occurred while running the job. %s Please contact support for help."
             .formatted(errorMsg));
+  }
+
+  /**
+   * Deserialization constructor used by {@link
+   * bio.terra.pipelines.dependencies.stairway.StairwayExceptionSerializer}. The message is already
+   * fully formatted, so it is passed through to the superclass without re-wrapping.
+   */
+  public InternalStepException(String message, List<String> causes) {
+    super(message, causes);
   }
 }
