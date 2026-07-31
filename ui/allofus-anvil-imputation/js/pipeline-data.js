0f6a8cbd282fd19cc1ac8337a42190d4b01e784a
Matt Bemis <mbemis@broadinstitute.org>
2026-07-31 08:07:39 -0400

    restyle s3 text

--- ui/allofus-anvil-imputation/js/pipeline-data.js
+++ ui/allofus-anvil-imputation/js/pipeline-data.js
@@ -215,7 +215,7 @@ const PIPELINES = {
       },
       {
         title: "Retrieve your results",
-        bodyHTML: `Download your results or have them delivered to a Google Cloud Storage bucket of your choice.<div class="how-step-note">Amazon S3 support is coming soon.</div>`,
+        bodyHTML: `Download your results or have them delivered to a Google Cloud Storage bucket of your choice.<div class="how-step-note"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg> Amazon S3 support is coming soon.</div>`,
         img: "img/step4-results.png",
         alt: "Retrieve your results",
       }
@@ -372,7 +372,7 @@ const PIPELINES = {
       },
       {
         title: "Retrieve your results",
-        bodyHTML: `Download your results or have them delivered to a Google Cloud Storage bucket of your choice.<div class="how-step-note">Amazon S3 support is coming soon.</div>`,
+        bodyHTML: `Download your results or have them delivered to a Google Cloud Storage bucket of your choice.<div class="how-step-note"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg> Amazon S3 support is coming soon.</div>`,
         img: "img/step4-results.png",
         alt: "Retrieve your results",
       }
