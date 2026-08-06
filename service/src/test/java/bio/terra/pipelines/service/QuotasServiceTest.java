ad9a44bdc4ea55815c15849b722746b2c65c54ce
Matt Bemis <mbemis@broadinstitute.org>
2026-08-06 15:01:12 -0400

    use new support email

--- service/src/test/java/bio/terra/pipelines/service/QuotasServiceTest.java
+++ service/src/test/java/bio/terra/pipelines/service/QuotasServiceTest.java
@@ -257,7 +257,7 @@ class QuotasServiceTest extends BaseEmbeddedDbTest {
     // Validate the exception message
     assertEquals(
         "Insufficient quota to run the pipeline. Quota available: 300, Minimum quota required: 500. "
-            + "Please email scientific-services-support@broadinstitute.org if you would like to request a quota increase.",
+            + "Please email data-science-services-support@broadinstitute.org if you would like to request a quota increase.",
         exception.getMessage());
   }
 
